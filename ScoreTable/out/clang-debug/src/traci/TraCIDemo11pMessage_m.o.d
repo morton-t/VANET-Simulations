6c00e8b4618f6dae3303fb1245e067fe86e1c053
morton-t <107581843+morton-t@users.noreply.github.com>
2024-01-04 18:01:07 -0600

    Add files via upload

---
+++ ScoreTable/out/clang-debug/src/traci/TraCIDemo11pMessage_m.o.d
@@ -0,0 +1,31 @@
+../out/clang-debug/src/traci/TraCIDemo11pMessage_m.o: \
+  traci/TraCIDemo11pMessage_m.cc traci/TraCIDemo11pMessage_m.h \
+  /home/user/Desktop/omnetpp-5.7/include/omnetpp.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/common_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/veins.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/FWMath.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/messages/BaseFrame1609_4_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/SimpleAddress_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/SimpleAddress.h
+
+traci/TraCIDemo11pMessage_m.h:
+
+/home/user/Desktop/omnetpp-5.7/include/omnetpp.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/common_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/veins.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/FWMath.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/messages/BaseFrame1609_4_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/SimpleAddress_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/SimpleAddress.h:
---
+++ ScoreTable/out/clang-debug/src/traci/TraCIDemoRSU11p.o.d
@@ -0,0 +1,163 @@
+../out/clang-debug/src/traci/TraCIDemoRSU11p.o: traci/TraCIDemoRSU11p.cc \
+  traci/TraCIDemoRSU11p.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/application/ieee80211p/DemoBaseApplLayer.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseApplLayer.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/veins.h \
+  /home/user/Desktop/omnetpp-5.7/include/omnetpp.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseLayer.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BatteryAccess.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseModule.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/HostState.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseBattery.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/PassedMessage.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/SimpleAddress.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/utility/Consts80211p.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/utility/ConstsPhy.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/messages/BaseFrame1609_4_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/SimpleAddress_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/common_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/messages/DemoServiceAdvertisement_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/FWMath.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/messages/DemoSafetyMessage_m.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/ChannelAccess.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/FindModule.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseMobility.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Move.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Heading.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mac/ieee80211p/DemoBaseApplLayerToMac1609_4Interface.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/NetwToMacControlInfo.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIMobility.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/ObstacleControl.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/Obstacle.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/world/annotations/AnnotationManager.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/utility/BBoxLookup.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/VehicleObstacleControl.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/MobileHostObstacle.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIBuffer.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConstants.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIColor.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConnection.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoord.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/VehicleSignal.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/EnumBitset.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICommandInterface.h \
+  /home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
+  traci/TraCIDemo11pMessage_m.h
+
+traci/TraCIDemoRSU11p.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/application/ieee80211p/DemoBaseApplLayer.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseApplLayer.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/veins.h:
+
+/home/user/Desktop/omnetpp-5.7/include/omnetpp.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseLayer.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BatteryAccess.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseModule.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/HostState.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseBattery.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/PassedMessage.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/SimpleAddress.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/utility/Consts80211p.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/utility/ConstsPhy.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/messages/BaseFrame1609_4_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/SimpleAddress_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/common_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/messages/DemoServiceAdvertisement_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Coord.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/FWMath.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/messages/DemoSafetyMessage_m.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/ChannelAccess.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/AntennaPosition.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/FindModule.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseMobility.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Move.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/modules/BaseWorldUtility.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/Heading.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mac/ieee80211p/DemoBaseApplLayerToMac1609_4Interface.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/NetwToMacControlInfo.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIMobility.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/connectionManager/NicEntry.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/utility/HasLogProxy.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/ObstacleControl.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/Obstacle.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/world/annotations/AnnotationManager.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/utility/BBoxLookup.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIColor.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoord.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/base/utils/EnumBitset.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/mobility/traci/TraCICommandInterface.h:
+
+/home/user/Desktop/veins-5.2/veins-veins-5.2/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
+
+traci/TraCIDemo11pMessage_m.h:
